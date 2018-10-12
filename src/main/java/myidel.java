67f3bc0a68fb18fb792f39eacef73751646b6d0e
jiangp2018 <jiangzon2018>
2018-10-12 12:21:51 +0800

    测试

---
+++ src/main/java/myidel.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class myidel
+        //<editor-fold desc="Description">
+{
+    //</editor-fold>
+    //region Description
+
+    //endregion
+    List<String> list = new ArrayList();
+    list.add("123");
+}
+
